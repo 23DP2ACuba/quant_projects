a8796d60dd1b0339c44c7b80e23a6a432364b1b6
14CPl0ver88 <166109704+23DP2ACuba@users.noreply.github.com>
2025-09-05 16:12:24 +0300

    Update ImpliedVolatilityDashboard.py

--- implied_volatility_trading_dashboard/src/ImpliedVolatilityDashboard.py
+++ implied_volatility_trading_dashboard/src/ImpliedVolatilityDashboard.py
@@ -9,16 +9,89 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from ibapi.contract import Contract
 from datetime import datetime
 import warnings
-from utils.DisplayUtils import *
+from utils import DisplayUtils
+from IBApp import IBApp
+
 warnings.filterwarnings('ignore')
 
 class ImpliedVolatilityDashboard:
     def __init__(self, root):
-        pass
-    def create_equity_contract():
-        pass
+        self.root = root
+        self.root.title("Implied Vol Dashboard")
+        self.root.geometey("1400x1200")
+        
+        self.option_data = None
+        self.volatility_data = None
+        self.current_implied_volatility = None
+        
+        self.ib_app = IBApp()
+        self.connected = False
+        
+        self.vol_annualization = 252
+        
+        self.setup_ui()
+        
+    def create_equity_contract(self, symbol):
+        contract = Contract()
+        contract.symbol = symbol.upper()
+        contract.secType = "STK"
+        contract.exchange = "SMART"
+        contract.currency = "USD"
+        return contract
+        
     def setup_ui(self):
-        pass
+        main_frame = ttk.Frame(self.root, padding = "10")
+        main_frame.grid(row = 0, column = 0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        
+        self.root.columncofig(0, weight=1)
+        self.root.rowcofig(0, weight=1)
+        
+        main_frame.columnconfigure(1, weight=1)
+        main_frame.rowconfigure(5, weight=1)
+        
+        conn_frame = ttk.LabelFrame(main_frame, text ="IB connection", padding="10")
+        conn_frame.grid(row=0, column=0, columnspan=2, sticky=(tk.W, tk.E), pady = (0, 10))
+        
+        ttk.Label(conn_frame, text="Host:").grid(row=0, column=0, padx=(0, 5))
+        self.host_var = tk.StringVar(value="127.0.0.1")
+        ttk.Entry(conn_frame, textvariable=self.host_var, width=15).grid(row=0, column=1, padx=(0, 10))
+        
+        ttk.Label(conn_frame, text="Port:").grid(row=0, column=0, padx=(0, 5))
+        self.port_var = tk.StringVar(value="7497")
+        ttk.Entry(conn_frame, textvariable=self.port_var, width=10).grid(row=0, column=3, padx=(0, 10))
+        
+        self.connect_btn = ttk.Button(conn_frame, text="Connect",command=self.connect_ib)
+        self.connect_btn.grid(row=0, column=4, padx=(0, 10))
+        
+        self.disconnect_btn = ttk.Button(conn_frame, text="Connect",command=self.disconnect_ib,state="disabled")
+        self.disconnect_btn.grid(row=0, column=5, padx=(0, 10))
+        
+        data_frame = tk.LabelFrame(main_frame, text="Data Querry", paddinx="5")
+        data_frame.grid(row=1, column=0, sticky=(tk.W, tk.E), pady=(0, 10))
+        
+        ttk.Label(data_frame, text="Symbol:").grid(row=0, column=0, padx=(0, 5))
+        self.symbol_var = tk.StringVar(value="SPY")
+        ttk.Entry(data_frame, textvariable=self.symbol_var, width=10).grid(row=0, column=1, padx=(0, 10))
+        
+        ttk.Label(data_frame, text="Duration:").grid(row=0, column=2, padx=(0, 5))
+        self.duration_var = tk.StringVar(value="2 Y")
+        ttk.Entry(data_frame, textvariable=self.duration_var, width=10).grid(row=0, column=3, padx=(0, 10))
+        
+        self.query_btn = ttk.Button(data_frame, text="Querry IB data",command=self.query_data, state="disabled")
+        self.query_btn.grid(row=0, column=4, padx=(0, 10))
+        
+        self.analyze_btn = ttk.Button(data_frame, text="Analyze Vol",command=self.analyze_volatility,state="disabled")
+        self.analyze_btn.grid(row=0, column=5, padx=(0, 10))
+        
+        vol_frame = ttk.LabelFrame(main_frame, text="Curr Emplyed Vol", padding="5")
+        vol_frame.grid(row=2, column=0, columnspan=2, sticky=(tk.W, tk.E), pady=(0, 10))
+        
+        ttk.Label(vol_frame, text="Curr IV").grid(row=0, column=0, padx=(0, 5))
+        self.current_vol_label = ttk.Label(vol_frame, text="N/A", font=("Arial", 12, "bold"))
+        self.current_vol_label.grid(row=0, column=1, padx=(0, 20))
+        
+        
+        
     def log_message(self):
         pass
     def connect_ib(self):
@@ -26,7 +99,7 @@ class ImpliedVolatilityDashboard:
             pass
     def disconnect_ib(self):
         pass
-    def querry_data(self):
+    def query_data(self):
         pass
     def process_implied_volatility(self):
         pass
