dc145e274cc1f965edda55e4d3417d4363bc8b27
14CPl0ver88 <166109704+23DP2ACuba@users.noreply.github.com>
2025-09-05 16:12:47 +0300

    Update IBApp.py

--- implied_volatility_trading_dashboard/src/IBApp.py
+++ implied_volatility_trading_dashboard/src/IBApp.py
@@ -9,14 +9,29 @@ class IBApp(EClient, EWrapper):
         self.connected = False
         self.historical_data = {}
         
-    def error(self):
-        pass
+    def error(self, reqId, errorCode, errorString):
+        print(f"Error: {reqId}, {errorCode}, {errorString}")
+        
 
     def nextValidId(self):
         pass
     
-    def historicalData(self):
-        pass
+    def historicalData(self, reqId, bar):
+        if reqId not in self.historical_data:
+            self.historical_data[reqId] = []
+        self.historical_data[reqId].append({
+            "date": bar.date,
+            "open": bar.open,
+            "high": bar.high,
+            "low": bar.low,
+            "close": bar.close,
+            "volume": bar.volume,
+        })
+        
     
-    def HistoricalDataEnd(self):
-        pass
+    def historicalDataEnd(self, reqId):
+        print(f"Historical data recieved for {reqId}")
+    
+    
+        
+        
