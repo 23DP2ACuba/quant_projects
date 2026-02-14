06298052ee6be523007b46554fc726300430ea2e
14CPl0ver88 <166109704+23DP2ACuba@users.noreply.github.com>
2026-02-14 13:34:09 +0200

    Update LatentSpaceModel.py

--- LatentVolHeston/LatentSpaceModel.py
+++ LatentVolHeston/LatentSpaceModel.py
@@ -1,12 +1,74 @@
 import torch
 from torch import nn
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+
+class DataStore(Dataset):
+    def __init__(self, data, feature_cols, target_col="RealizedVol", seq_len=60):
+        
+        self.seq_len = seq_len
+        
+        self.features = data[feature_cols].values
+        
+        self.target = data[target_col].shift(-1).values
+        
+        self.features = self.features[:-1]
+        self.target = self.target[:-1]
+
+    def __len__(self):
+        return len(self.features) - self.seq_len
+
+    def __getitem__(self, idx):
+        x = self.features[idx: idx + self.seq_len]
+        y = self.target[idx + self.seq_len - 1]
+
+        x = torch.tensor(x, dtype=torch.float32)
+        y = torch.tensor(y, dtype=torch.float32)
+
+        return x, y
+
 
 class Encoder(nn.Module):
-    def __init__(self, num_features, latent_dim, n_heads=4, n_layers=2, dropout=0.1):
-        super(Encoder, self).__init__()
-    
+    def __init__(self, input_dim=11, num_layers=3, d_model=64, 
+                 nhead=4, seq_len=100):
+        super().__init__()
+
+        self.fc_in = nn.Linear(input_dim, d_model)
+
+        self.pos_embd = nn.Parameter(
+            torch.randn(1, seq_len, d_model)
+        )
+
+        enc_layer = nn.TransformerEncoderLayer(
+            d_model=d_model,
+            nhead=nhead,
+            dim_feedforward=4*d_model,
+            dropout=0.1,
+            activation="gelu",
+            batch_first=True,
+            norm_first=True
+        )
+
+        self.transformer_encoder = nn.TransformerEncoder(
+            enc_layer,
+            num_layers=num_layers
+        )
+
+        self.fc_out = nn.Linear(d_model, 1)
+
     def forward(self, x):
-        pass
+        batch_size, seq_len, _ = x.shape
+
+        x = self.fc_in(x)
+        x = x + self.pos_embd[:, :seq_len, :]
+
+        x = self.transformer_encoder(x)
+
+        z_t = x[:, -1, :]         
+
+        y_pred = self.fc_out(z_t)
+
+        return y_pred, z_t
     
 
 class Decoder(nn.Module):
@@ -36,4 +98,5 @@ class NMVMDistribution:
             W = torch.ones(n_samples)
         else:
             raise ValueError("Unsupported W distribution")
+
         return W
