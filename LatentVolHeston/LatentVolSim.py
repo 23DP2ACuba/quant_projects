614530af8562b3f88a997524c39e875c2c41092f
14CPl0ver88 <166109704+23DP2ACuba@users.noreply.github.com>
2026-03-19 14:39:11 +0200

    Refactor LatentVolSimulation
    Refactor LatentVolSim class to LatentVolSimulation and enhance simulation method.

--- LatentVolHeston/LatentVolSim.py
+++ LatentVolHeston/LatentVolSim.py
@@ -2,7 +2,7 @@ import numpy as np
 from LatentSpaceFeatures import LatentSpaceVol
 from LatentSpaceModel import NMVMDistribution
 
-class LatentVolSim:
+class LatentVolSimulation:
   def __init__(self, model, n_paths, n_steps, T, mu):
     self.model = model
     self.n_paths = n_paths
@@ -12,12 +12,21 @@ class LatentVolSim:
     self.mu = mu
     self.price_paths = np.zeros((n_paths, n_steps+1))
     self.v_t = model.sample(n_paths, n_steps)
+    print("v_t stats:", self.v_t.min(), self.v_t.max())
 
   def simulate(self, S0, plot=False):
     self.price_paths[:, 0] = S0
-    for t in range(2):
+    print(self.dt)
+    for t in range(self.n_steps):
       x = np.log(self.price_paths[:, t])
       x_next = x + (self.mu - 0.5*self.v_t[:, t])*self.dt + np.sqrt(self.v_t[:, t])*self.Z[:, t] * np.sqrt(self.dt)
-      print((self.mu - 0.5*self.v_t[:, t]))
       self.price_paths[:, t+1] = np.exp(x_next)
-    return self.price_paths
+    return self.price_paths.T
+
+  def plot(self):
+    paths = self.price_paths.T
+    plt.plot(paths[:, 0])
+    plt.show()
+
+
+
